d2e9f8b7ce1eda2fb0e38dbcc0a298076a1ca290
NSVVManikanta <101240994+NSVVManikanta@users.noreply.github.com>
2022-03-09 20:37:46 +0530

    Add files via upload

---
+++ models/songs.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+const sequelize= require('../util/database');
+const song= sequelize.define('songs',{
+    id:{
+        type : Sequelize.INTEGER,
+        autoIncrement : true,
+        allowNull: false,
+        primaryKey : true,
+    },
+    title:{
+        type : Sequelize.STRING,
+        allowNull : false,
+        unique: true
+    },
+    length:{
+        type : Sequelize.TIME,
+        allowNull : false,
+    },
+    composers:{
+        type : Sequelize.STRING,
+        allowNull : false,
+       
+    },
+    singers:{
+        type : Sequelize.STRING,
+        allowNull : false,
+    },
+    lyricists:{
+        type : Sequelize.STRING,
+        allowNull : false,
+    },
+   
+});
+
+
+module.exports=song;
