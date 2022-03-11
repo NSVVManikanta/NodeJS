8d95c0bbe3b6daf326544a45df702cd38dcfb49f
NSVVManikanta <101240994+NSVVManikanta@users.noreply.github.com>
2022-03-11 20:05:38 +0530

    Update album.js

--- controllers/album.js
+++ controllers/album.js
@@ -1,11 +1,44 @@
 const albums = require('../models/albums');
 const songs = require('../models/songs');
+const {sequelize} = {albums,songs};
+const joi = require('@hapi/joi');
 
 const welcome = (req, res) => {
   res.status(200).send("Music Library API. Built by manikanta.");
 };
 
-const create = (req, res) => {
+/*const create = async(req, res) => {
+  let t ;
+  try{
+      t = await sequelize.transaction(); 
+      const Album =await albums.create({
+        title:req.body.title,
+        year:req.body.year
+      }, {transaction:t});
+      await albums.createsongs({
+        title:req.body.title,
+        length:req.body.length,
+        composers:req.body.composers,
+        singers:req.body.singers,
+        lyricists:req.body.lyricists,
+        albumId:req.body.albumId,
+      },{transaction:t});
+      await t.commit();
+      res.send(Album);
+      } catch (error) {
+       await t.rollback()
+     }
+    };*/
+    const schema = joi.object({
+      title:joi.string().min(3).required()
+    });
+    const create = (req, res) => {
+      const schemaerr = schema.validate(
+        req.body.title
+        );
+      if(schemaerr.error){
+        return res.send(schemaerr.error);
+      }else{
       albums.create({
         title:req.body.title,
         year:req.body.year
@@ -14,7 +47,8 @@ const create = (req, res) => {
       }).catch(err=>{
         console.log(err);
       });
-    };
+    }
+  };
 
 const list = (req, res) => {
     albums.findAll({
@@ -56,4 +90,4 @@ const list = (req, res) => {
     });
   };
 
-  module.exports = { welcome,create, list,  update, deleteAlbum };
+  module.exports = { welcome, list, create, update, deleteAlbum };
