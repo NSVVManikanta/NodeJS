8d7fd02e02d6254e723e8723fd027b48d2df5563
NSVVManikanta <101240994+NSVVManikanta@users.noreply.github.com>
2022-03-14 00:51:04 +0530

    Update album.js

--- controllers/album.js
+++ controllers/album.js
@@ -1,75 +1,98 @@
 const albums = require('../models/albums');
 const songs = require('../models/songs');
-const {sequelize} = {albums,songs};
-const joi = require('@hapi/joi');
+const { sequelize } = require('../models/songs');
+const Joi = require('joi');
 
+//Welcome Page
 const welcome = (req, res) => {
   res.status(200).send("Music Library API. Built by manikanta.");
 };
 
-/*const create = async(req, res) => {
-  let t ;
-  try{
-      t = await sequelize.transaction(); 
-      const Album =await albums.create({
-        title:req.body.title,
-        year:req.body.year
-      }, {transaction:t});
-      await albums.createsongs({
-        title:req.body.title,
-        length:req.body.length,
-        composers:req.body.composers,
-        singers:req.body.singers,
-        lyricists:req.body.lyricists,
-        albumId:req.body.albumId,
-      },{transaction:t});
-      await t.commit();
-      res.send(Album);
-      } catch (error) {
-       await t.rollback()
-     }
-    };*/
-    const schema = joi.object({
-      title:joi.string().min(3).required()
-    });
-    const create = (req, res) => {
-      const schemaerr = schema.validate(
-        req.body.title
-        );
-      if(schemaerr.error){
-        return res.send(schemaerr.error);
-      }else{
-      albums.create({
-        title:req.body.title,
-        year:req.body.year
-      }).then((album) => {
-        res.status(200).send(album);
-      }).catch(err=>{
-        console.log(err);
-      });
-    }
-  };
+// const objectSchema =Joi.object({
+//   title :Joi.string().alphanum().min(2).max(250).required(),
+//   composers:Joi.string().alphanum().min(2).max(250).required(),
+//   singers:Joi.string().alphanum().min(2).max(250).required(),
+//   lyricists:Joi.string().alphanum().min(2).max(250).required(),
+// });
+const schema = Joi.object({
+  title :Joi.string().alphanum().min(2).max(250).required(),
+  year:Joi.number().integer().min(1900).max(2022).required(),
+  // songs:Joi.array().items(objectSchema).min(1).required(),
+});
+// Create Album
+const create = async (req, res) => {
+  const dataToValidate = {
+    title:req.body.title,
+    year:req.body.year,
+    // songs:req.body.songs,
+  }
 
+  const schemaerr = schema.validate(dataToValidate);
+  if(schemaerr.error){
+    return res.send(schemaerr.error);
+  }else{
+  let t;
+  try {
+    t = await sequelize.transaction();
+    console.log("entered", t);
+    const Album = await albums.create({
+      title: req.body.title,
+      year: req.body.year
+    }, { transaction: t });
+    const songData = req.body.songs;
+    const preparedSongObjArr = songData.map(song => {
+      song.albumId = Album.id
+      return song
+    });
+    console.log(preparedSongObjArr);
+    await songs.bulkCreate(preparedSongObjArr, { transaction: t });
+    await t.commit();
+    res.send(Album);
+  } catch (error) {
+    await t.rollback()
+    console.log(error);
+  }
+}
+};
+
+//Album List
 const list = (req, res) => {
-    albums.findAll({
-      include: [
-        {
-          model: songs
-        }
-      ],
-      order: [
-        ["createdAt", "DESC"],
-      ],
-    }).then((albums) => {
-      res.status(200).send(albums);
-    }).catch(err=>{
-      console.log(err);
-    });
-  };
+  albums.findAll({
+    include: [
+      {
+        model: songs
+      }
+    ],
+    order: [
+      ["createdAt", "DESC"],
+    ],
+  }).then((albums) => {
+    res.status(200).send(albums);
+  }).catch(err => {
+    console.log(err);
+  });
+};
 
-  const update = (req, res) => {
-    const { albumId } = req.params;
-    albums.update(req.body, { where: { id: albumId } }).then(
+//Update Album
+const schema1 = Joi.object({
+  title :Joi.string().alphanum().min(2).max(250).required(),
+  year:Joi.number().integer().min(1900).max(2022).required(),
+});
+const update = (req, res) => {
+  const dataToValidate1 = {
+    title:req.body.title,
+    year:req.body.year,
+  }
+  const schemaerr1 = schema1.validate(dataToValidate1);
+  if(schemaerr1.error){
+    return res.send(schemaerr1.error);
+  }else{
+  const { albumId } = req.params;
+  albums.update({
+    title: req.body.title,
+    year: req.body.year
+  },
+    { where: { id: albumId } }).then(
       ([numOfRowsUpdated]) => {
         if (numOfRowsUpdated === 0) {
           res.status(404).json({ error: "The album does not exist." });
@@ -78,16 +101,18 @@ const list = (req, res) => {
         }
       }
     );
-  };
+};
+};
 
-  const deleteAlbum = (req, res) => {
-    const { albumId } = req.params;
-    albums.destroy({ where: { id: albumId } }).then((numOfRowsDeleted) => {
-      if (numOfRowsDeleted === 0) {
-        res.status(404).json({ error: "The album does not exist." });
-      }
-      res.status(204).json(numOfRowsDeleted);
-    });
-  };
+//Delete Album
+const deleteAlbum = (req, res) => {
+  const { albumId } = req.params;
+  albums.destroy({ where: { id: albumId } }).then((numOfRowsDeleted) => {
+    if (numOfRowsDeleted === 0) {
+      res.status(404).json({ error: "The album does not exist." });
+    }
+    res.status(204).json(numOfRowsDeleted);
+  });
+};
 
-  module.exports = { welcome, list, create, update, deleteAlbum };
+module.exports = { welcome, list, create, update, deleteAlbum };
